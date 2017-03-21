7ef1b35d1cd6bafa1256d9295ad2876a9a6d5b02
Michael Howell <michael@notriddle.com>
2017-03-20 17:15:10 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# chaliy-chocolatey-packages
+
+## Push
+
+```
+push package
+```
+
+## Test
+
+```
+cd package
+choco pack
+choco install package -s ..
+```
---
+++ push.ps1
@@ -0,0 +1,54 @@
+param(
+    [ValidateSet('gnuplot', 'thrift', 'openldap', 'boot2docker', 'octave', 'activator', 'miniconda','miniconda3')]
+    [Parameter(Mandatory = $true)]
+    [ValidateNotNullOrEmpty()]
+    [string]$Package,
+    [switch]$Confirm=$True
+)
+
+# choco apiKey -k YOURS-VERY-OWN-API-KEY -source https://chocolatey.org/
+
+$here = (Split-Path -parent $MyInvocation.MyCommand.Definition)
+
+pushd
+
+try {
+
+    cd $here/$Package
+
+    rm *.nupkg
+    choco pack
+
+    gci *.nupkg | %{
+        $PackagePath = $_
+        if ($Confirm){
+           $ConfirmText = @"
+Confirm
+Are you sure you want to push $PackagePath to chocolatey?
+"@
+            Write-Host $ConfirmText
+            $answer = Read-Host @"
+[Y] Yes [N] No [?] Help (default is "N")
+"@
+            switch ($Answer)
+            {
+                "Y" {
+                    Write-Host Push $PackagePath
+                    choco push $PackagePath
+                    rm $PackagePath
+                }
+                "" { }
+                "N" {  }
+                "?" { Write-Host @"
+Y - Continue with only the next step of the operation.
+N - Skip this operation and proceed with the next operation.
+"@
+                }
+            }
+        }
+    }
+} finally {
+    popd
+}
+
+git add -A
