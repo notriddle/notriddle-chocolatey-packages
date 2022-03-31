a86682ff39c38a2500d386838f3d2092c352fa1a
Michael Howell <michael@notriddle.com>
2022-03-31 15:01:50 -0700

    Get rid of pthat switch thing

--- push.ps1
+++ push.ps1
@@ -25,31 +25,9 @@ try {
 
     gci *.nupkg | %{
         $PackagePath = $_
-        if ($Confirm){
-           $ConfirmText = @"
-Confirm
-Are you sure you want to push $PackagePath to chocolatey?
-"@
-            Write-Host $ConfirmText
-            $answer = Read-Host @"
-[Y] Yes [N] No [?] Help (default is "N")
-"@
-            switch ($Answer)
-            {
-                "Y" {
-                    Write-Host Push $PackagePath
-                    choco push $PackagePath
-                    rm $PackagePath
-                }
-                "" { }
-                "N" {  }
-                "?" { Write-Host @"
-Y - Continue with only the next step of the operation.
-N - Skip this operation and proceed with the next operation.
-"@
-                }
-            }
-        }
+        Write-Host Push $PackagePath
+        choco push $PackagePath
+        rm $PackagePath
     }
 } finally {
     popd
